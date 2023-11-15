de538c35d634348a7d3f66bdfc73caff6e162b41
João Vitor Silva <85369585+Joaohue@users.noreply.github.com>
2023-11-15 12:47:06 -0300

    Esse daqui é o caso medio para o insertion sort

---
+++ MEDIO - INSERTION.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+    long tempoExecucao;
+    long trocas;
+    long comparacoes;
+} Estatisticas;
+
+void insertionSort(int arr[], int n, Estatisticas *stats) {
+    int i, chave, j;
+    stats->trocas = 0;
+    stats->comparacoes = 0;
+
+    clock_t inicio = clock();
+
+    for (i = 1; i < n; i++) {
+        chave = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > chave) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+
+            stats->trocas++;
+            stats->comparacoes++;
+        }
+        arr[j + 1] = chave;
+        stats->trocas++;
+    }
+
+    clock_t fim = clock();
+
+    stats->tempoExecucao = ((double)(fim - inicio) / CLOCKS_PER_SEC) * 1000;
+}
+
+void printArray(int arr[], int n) {
+	int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+	int i;
+    // Tamanho do array aqui que ira trocar para tamanhos de 10^3 / 10^4 / 10^5
+    int tamanho = 1000;
+
+    // Array de tamanho 1.000
+    int arr[tamanho];
+
+    // Preenchendo o array com valores aleatórios (ou conforme necessário)
+    for (i = 0; i < tamanho; i++) {
+        arr[i] = rand(); // Para valores aleatórios, inclua a biblioteca <stdlib.h>
+    }
+
+    printf("Array original (tamanho %d): \n", tamanho);
+    printArray(arr, tamanho);
+
+    // Estrutura para armazenar estatísticas
+    Estatisticas stats;
+
+    insertionSort(arr, tamanho, &stats);
+
+    printf("Array ordenado: \n");
+    printArray(arr, tamanho);
+
+    // Imprime estatísticas
+    printf("Estatisticas:\n");
+    printf("Tempo de Execucao: %ld ms\n", stats.tempoExecucao);
+    printf("Quantidade de Trocas: %ld\n", stats.trocas);
+    printf("Quantidade de Comparacoes: %ld\n", stats.comparacoes);
+
+    return 0;
+}
+
