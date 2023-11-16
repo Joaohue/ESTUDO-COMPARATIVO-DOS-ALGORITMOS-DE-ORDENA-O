e1ee01c1b8f9523dfed0751641f54cce02f41534
João Vitor Silva <85369585+Joaohue@users.noreply.github.com>
2023-11-16 18:52:40 -0300

    Update MEDIO - Heap Sort.c

--- MEDIO - Heap Sort.c
+++ MEDIO - Heap Sort.c
@@ -10,20 +10,19 @@ typedef struct {
 
 void heapify(int arr[], int n, int i, Estatisticas *stats) {
     int maior = i;
-    int esquerda = 2 * i + 1;
-    int direita = 2 * i + 2;
-
+    int esq = 2 * i + 1;
+    int dir = 2 * i + 2;
 
     stats->comparacoes++;
 
-    if (esquerda < n && arr[esquerda] > arr[maior]) {
-        maior = esquerda;
+    if (esq < n && arr[esq] > arr[maior]) {
+        maior = esq;
     }
 
     stats->comparacoes++;
 
-    if (direita < n && arr[direita] > arr[maior]) {
-        maior = direita;
+    if (dir < n && arr[dir] > arr[maior]) {
+        maior = dir;
     }
 
     if (maior != i) {
@@ -39,13 +38,11 @@ void heapify(int arr[], int n, int i, Estatisticas *stats) {
 
 void heapSort(int arr[], int n, Estatisticas *stats) {
 	int i;
-
     for (i = n / 2 - 1; i >= 0; i--) {
         heapify(arr, n, i, stats);
     }
 
     for (i = n - 1; i >= 0; i--) {
-   
         int temp = arr[0];
         arr[0] = arr[i];
         arr[i] = temp;
@@ -64,27 +61,27 @@ void printArray(int arr[], int n) {
 
 int main() {
 	int i;
-    int tamanho = 1000; // Tamanho do array
+    int n = 1000; // Tamanho do array
 
     // Array de tamanho 1.000
-    int arr[tamanho];
+    int arr[n];
 
-    // Preenchendo o array com valores aleatórios (ou conforme necessário)
-    for (i = 0; i < tamanho; i++) {
+    // Preenchendo o array com valores aleatÃ³rios (ou conforme necessÃ¡rio)
+    for (i = 0; i < n; i++) {
         arr[i] = rand();
     }
 
-    printf("Array original (tamanho %d): \n", tamanho);
-    printArray(arr, tamanho);
+    printf("Array original (tamanho %d): \n", n);
+    printArray(arr, n);
 
     Estatisticas stats;
 
     clock_t inicio = clock();
-    heapSort(arr, tamanho, &stats);
+    heapSort(arr, n, &stats);
     clock_t fim = clock();
 
     printf("Array ordenado: \n");
-    printArray(arr, tamanho);
+    printArray(arr, n);
 
     printf("Estatisticas:\n");
     printf("Tempo de Execucao: %ld ms\n", ((fim - inicio) * 1000) / CLOCKS_PER_SEC);
@@ -93,4 +90,3 @@ int main() {
 
     return 0;
 }
-
