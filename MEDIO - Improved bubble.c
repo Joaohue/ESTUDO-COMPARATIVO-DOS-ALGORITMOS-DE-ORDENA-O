236c0941e0066249919880ec36098f98b506f55b
João Vitor Silva <85369585+Joaohue@users.noreply.github.com>
2023-11-17 15:23:09 -0300

    Add files via upload

---
+++ MEDIO - Improved bubble.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+typedef struct {
+    long tempoExecucao;
+    long trocas;
+    long comparacoes;
+} Estatisticas;
+
+void improvedBubbleSort(int arr[], int n, Estatisticas *stats) {
+    int i, j;
+    stats->trocas = 0;
+    stats->comparacoes = 0;
+
+    clock_t inicio = clock();
+
+    int trocasRealizadas = 0;
+
+    for (i = 0; i < n-1; i++) {
+        int trocasNestaPassagem = 0;
+
+        for (j = 0; j < n-i-1; j++) {
+            stats->comparacoes++;
+
+            if (arr[j] > arr[j+1]) {
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+
+                stats->trocas++;
+                trocasRealizadas++;
+                trocasNestaPassagem++;
+            }
+        }
+
+        if (trocasNestaPassagem == 0) {
+            break;
+        }
+    }
+
+    clock_t fim = clock();
+
+    stats->tempoExecucao = ((double)(fim - inicio) / CLOCKS_PER_SEC) * 1000;
+}
+
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+int main(int argc, char *argv[]) {
+    int i;
+    // Tamanho do array aqui que irá trocar para tamanhos de 10^3 / 10^4 / 10^5
+    int tamanho = 1000;
+
+    int arr[tamanho];
+    for (i = 0; i < tamanho; i++) {
+        arr[i] = rand();
+    }
+
+    printf("Array original: \n", tamanho);
+    printArray(arr, tamanho);
+
+    Estatisticas stats;
+
+    improvedBubbleSort(arr, tamanho, &stats);
+
+    printf("Array ordenado: \n");
+    printArray(arr, tamanho);
+
+    // Imprime estatísticas
+    printf("Estatisticas:\n");
+    printf("Tempo de Execucao: %ld ms\n", stats.tempoExecucao);
+    printf("Quantidade de Trocas: %ld\n", stats.trocas);
+    printf("Quantidade de Comparacoes: %ld\n", stats.comparacoes);
+
+    return 0;
+}
+
